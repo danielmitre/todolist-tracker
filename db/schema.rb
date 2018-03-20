94155600337a1f505b7e615120a27ad562d8fb21
mitre <danielbgmitre@live.com>
2018-03-20 15:49:40 -0300

    Trying to fix heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180319153158) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "atividades", force: :cascade do |t|
     t.string "nome"
     t.text "descricao"
